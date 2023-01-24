e36932614b990515f854e2faecd4b37d96cfb804
Eduardo Souza <edcampossouza@gmail.com>
2023-01-23 21:50:46 -0300

    Initial commit
    set up express app

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 3500;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, console.log(`app listening on port ${PORT}`));
