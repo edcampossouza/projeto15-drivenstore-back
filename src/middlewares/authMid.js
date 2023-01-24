49b7500460e89d58a2f1a0bd28592cecec640606
Schweidzon <dannielsch@gmail.com>
2023-01-24 16:32:41 -0300

    Adicionando middlewares para verificar no registro se o email ja esta cadastrado e pro login para verificar email e senha que o usuario mandou

--- src/middlewares/authMid.js
+++ src/middlewares/authMid.js
@@ -1,4 +1,6 @@
-import db from "../config/database";
+import { v4 as uuidV4 } from "uuid"
+import db from "../config/database.js";
+import bcrypt from 'bcrypt'
 
 export async function authValidation(req, res, next) {
     const user = req.body
@@ -8,17 +10,64 @@ export async function authValidation(req, res, next) {
     try {
         const checkSession = await db.collection("sessions").findOne({ token })
 
-        if(!checkSession) return res.status(401).send("Você não tem autorização")
+        if (!checkSession) return res.status(401).send("Você não tem autorização")
 
-        const checkUserExist = await db.collection("users").findOne({email: user.email})
+        const checkUserExist = await db.collection("users").findOne({ email: user.email })
 
-        if(checkUserExist) return res.status(409).send("Email já cadastrado")
+        if (checkUserExist) return res.status(409).send("Email já cadastrado")
 
         res.locals.session = checkSession
-        
+
         next()
     } catch (error) {
         console.log(error.message)
         return res.status(500).send(error.message)
     }
+}
+
+export async function authSignUp(req, res, next) {
+    const user = req.body
+
+    try {
+        const checkUserExist = await db.collection("users").findOne({email: user.email})
+
+        if (checkUserExist) return res.status(401).send("Email já cadastrado")
+
+        next()
+        
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).send(error.message)
+        
+    }
+   
+
+}
+
+export async function authSignIn(req, res, next) {
+    const user = req.body
+
+    const token = uuidV4()
+
+    res.locals.token = token
+
+    try {
+
+        const checkUserExist = await db.collection("users").findOne({ email: user.email })
+
+        if (!checkUserExist) return res.status(404).send("Usuário ou senha incorretos")
+
+        const matchPassword = bcrypt.compareSync(user.password, checkUserExist.password)
+
+        if (!matchPassword) return res.status(404).send("Usuário ou senha incorretos")
+
+        await db.collection("sessions").insertOne({ userId: checkUserExist._id, token, user: checkUserExist.name })
+
+        next()
+
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).send(error.message)
+    }
+
 }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,11 +2,12 @@ import { Router } from 'express'
 import { registerUserSchema, loginSchema } from '../schemas/authSchemas.js'
 import { validateSchema } from '../middlewares/validateSchemas.js'
 import { signIn, signUp } from '../controllers/authControllers.js'
+import { authSignIn } from '../middlewares/authMid.js'
 
 
 const authRoutes = Router()
 
 authRoutes.post("/sign-up", validateSchema(registerUserSchema) ,signUp)
-authRoutes.post("/sign-in", validateSchema(loginSchema),signIn)
+authRoutes.post("/sign-in", validateSchema(loginSchema), authSignIn,signIn)
 
 export default authRoutes
