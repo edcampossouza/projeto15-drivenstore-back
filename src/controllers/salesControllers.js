2bf48edc10f1bdf021433d84e9d9eb1de16d3ae4
Eduardo Souza <edcampossouza@gmail.com>
2023-01-25 00:08:19 -0300

    feat: checkout

--- src/controllers/salesControllers.js
+++ src/controllers/salesControllers.js
@@ -48,6 +48,69 @@ export async function deleteCartItem(req, res) {
   }
 }
 
+//observacao: a funcao abaixo nao utiliza transactions
+//podem ocorrer race conditions
 export async function checkout(req, res) {
-  return res.send("em implementacao")
+  const { userId } = res.locals.session;
+  try {
+    const cartItems = await db
+      .collection("cart")
+      .find({ userID: ObjectId(userId) })
+      .toArray();
+
+    if (!cartItems || !cartItems.length) {
+      return res.status(404).send("Voce nao tem nenhum livro no seu carrinho");
+    }
+    const toUpdateStock = [];
+    const orderItems = [];
+    let totalPrice = 0;
+    for (const cartItem of cartItems) {
+      const book = await db
+        .collection("books")
+        .findOne({ _id: cartItem.bookID });
+      if (!book) {
+        return res.status(409).send(`Livro ${cartItem.bookID} nao encontrado`);
+      }
+      const itemPrice = cartItem.quantity * book.price;
+      cartItem.totalPrice = itemPrice;
+      totalPrice += itemPrice;
+      orderItems.push({
+        bookId: cartItem.bookID,
+        totalPrice: cartItem.totalPrice,
+        quantity: cartItem.quantity,
+      });
+      if (book.type === "physical") {
+        if (book.stock < cartItem.quantity) {
+          return res
+            .status(409)
+            .send(
+              `Fora de estoque: ${book.title}: Estoque: ${book.stock}. Qtd. requisitada: ${cartItem.quantity}`
+            );
+        } else {
+          toUpdateStock.push(cartItem);
+        }
+      }
+    }
+    // atualiza os saldos dos livros fisicos
+    for (const item of toUpdateStock) {
+      db.collection("books").updateOne(
+        { _id: item.bookID },
+        { $inc: { stock: -item.quantity } }
+      );
+    }
+
+    const order = {
+      userId: ObjectId(userId),
+      totalPrice,
+      books: orderItems,
+      datetime: Date.now(),
+    };
+
+    db.collection("orders").insertOne(order);
+    db.collection("cart").deleteMany({ userID: ObjectId(userId) });
+
+    return res.status(200).send(order);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 }
